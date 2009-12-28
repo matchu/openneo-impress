3da8ac430da78f527b0e042e13ae0d9e9d32918b
Matchu <matchu1993@gmail.com>
2009-12-28 01:48:29 -0500

    added hardcoded pet, species lists to later be replaced with db

--- lib/color.class.php
+++ lib/color.class.php
@@ -2,5 +2,14 @@
 require_once dirname(__FILE__).'/pet_attribute.class.php';
 
 class Wearables_Color extends Wearables_PetAttribute {
+  function getName() {
+    $all_color_names = self::all_color_names();
+    $returning = $all_color_names[$this->getId()-1];
+    return $returning ? $returning : parent::getName();
+  }
+  
+  static function all_color_names() {
+    return explode("\n", file_get_contents(dirname(__FILE__).'/colors.txt'));
+  }
 }
 ?>
--- lib/species.class.php
+++ lib/species.class.php
@@ -2,16 +2,14 @@
 require_once dirname(__FILE__).'/pet_attribute.class.php';
 
 class Wearables_Species extends Wearables_PetAttribute {
-  public function getName() {
-    $species_list = Wearables_AMF::getApplicationData()->species;
-    foreach($species_list as $species_obj) {
-      $species_data = $species_obj->getAMFData();
-      if($species_data->id == $this->getId()) {
-        return ucfirst(strtolower($species_data->type));
-      }
-    }
-    // if all list items failed
-    return parent::getName();
+  function getName() {
+    $all_species_names = self::all_species_names();
+    $returning = $all_species_names[$this->getId()-1];
+    return $returning ? $returning : parent::getName();
+  }
+  
+  static function all_species_names() {
+    return explode("\n", file_get_contents(dirname(__FILE__).'/species.txt'));
   }
 }
 ?>
