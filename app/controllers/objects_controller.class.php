43a0c86a51d89b8a693585f477eb862d21ca6955
Matchu <matchu1993@gmail.com>
2010-01-17 21:39:43 -0500

    add search functionality to object backend

--- app/controllers/objects_controller.class.php
+++ app/controllers/objects_controller.class.php
@@ -6,15 +6,30 @@ class Pwnage_ObjectsController extends PwnageCore_Controller {
   }
   
   public function index() {
-    $this->requireParamArray($this->get, 'ids');
-    $ids = implode(', ', array_map('intval', $this->get['ids']));
     $attributes = array(
       'id', 'name', 'thumbnail_url', 'description', 'type', 'rarity',
         'rarity_index', 'price', 'weight_lbs'
     );
+    if(isset($this->get['ids'])) {
+      $this->requireParamArray($this->get, 'ids');
+      $ids = implode(', ', array_map('intval', $this->get['ids']));
+      $where = 'id IN ('.$ids.')';
+    } elseif(isset($this->get['search'])) {
+      $search = $this->get['search'];
+      $db = PwnageCore_Db::getInstance();
+      if(preg_match('/^"(.+)"$/', $search, $matches)) {
+        $search = $matches[1];
+        $where = 'name = '.$db->quote($search);
+      } else {
+        $like = '%'.str_replace('%', '\%', $search).'%';
+        $where = 'name LIKE '.$db->quote($like);
+      }
+    } else {
+      throw new Pwnage_BadRequestException('$id or $search required');
+    }
     $objects = Pwnage_Object::all(array(
       'select' => implode(', ', $attributes),
-      'where' => 'id IN ('.$ids.')'
+      'where' => $where
     ));
     $this->respondWith($objects, $attributes);
   }
