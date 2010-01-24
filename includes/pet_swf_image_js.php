35f46504bcd01f4b3b2c2d9e00ecac310971a1d0
Matchu <matchu1993@gmail.com>
2010-01-24 15:53:21 -0500

    remove a few old components

--- includes/pet_swf_image_js.php
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-<script type="text/javascript" src="../assets/js/swfobject/swfobject.js"></script>
-<script type="text/javascript" src="../assets/js/pet_swf_image.js"></script>
